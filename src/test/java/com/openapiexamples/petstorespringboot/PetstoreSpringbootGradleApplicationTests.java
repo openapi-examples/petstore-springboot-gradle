6542de1abe5bdf92fc9ce6bb2059886baff5a898
Francis Novilla <novillf1@anz.com>
2024-03-17 20:01:52 +1300

    feat: initial implementation of petstore api

--- src/test/java/com/openapiexamples/petstorespringboot/PetstoreSpringbootGradleApplicationTests.java
+++ src/test/java/com/openapiexamples/petstorespringboot/PetstoreSpringbootGradleApplicationTests.java
@@ -1,4 +1,4 @@
-package com.openapiexamples.petstorespringbootgradle;
+package com.openapiexamples.petstorespringboot;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
